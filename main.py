bb1a5ecece866e7b836051797fe40d8368990926
saeidyazdi7 <76227269+saeidyazdi7@users.noreply.github.com>
2023-07-16 19:05:06 +0330

    1

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-row = 1
-while row <= 5:
-    print("*" * row)
-    row += 1
+n = int(input("Enter:"))
+i = 1
+c = 0
+while i < n:
+    if n % i == 0:
+        c += i
+    i += 1
+if c == n:
+    print("Yes")
