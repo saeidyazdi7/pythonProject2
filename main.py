693e5ea5facdd8e1b5ae5656f796d34040a1d359
saeidyazdi7 <76227269+saeidyazdi7@users.noreply.github.com>
2023-07-16 17:08:27 +0330

    2

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-dic = {}
-key = input("Keyword:")
-means = input("Mean:").split(",")
-dic[key] = means
-word = input("Enter:")
-print("Mean:", dic.get(word))
+mark = int(input("mark :"))
+if mark >= 15 and mark <= 20:
+    print("A")
+elif mark >= 10 and mark < 15:
+    print("B")
+elif mark >= 5 and mark < 10:
+    print("C")
+elif mark >= 0 and mark < 5:
+    print("D")
+else:
+    print("Hosh!")
