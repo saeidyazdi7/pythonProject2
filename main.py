88c0e5ab9951a31372e81391d20b63134315ead4
saeidyazdi7 <76227269+saeidyazdi7@users.noreply.github.com>
2023-07-16 17:09:36 +0330

    2

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-mark = int(input("mark :"))
-if mark >= 15 and mark <= 20:
+mark = int(input("Mark :"))
+if 15 <= mark <= 20:
     print("A")
-elif mark >= 10 and mark < 15:
+elif 10 <= mark < 15:
     print("B")
-elif mark >= 5 and mark < 10:
+elif 5 <= mark < 10:
     print("C")
-elif mark >= 0 and mark < 5:
+elif 0 <= mark < 5:
     print("D")
 else:
     print("Hosh!")
