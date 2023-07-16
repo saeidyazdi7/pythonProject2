d22a43a7dcc3b1aef9906f2e7f6e48a6a8d80ba6
saeidyazdi7 <76227269+saeidyazdi7@users.noreply.github.com>
2023-07-16 23:03:52 +0330

    1

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-i = 0
-while i < 10:
-    i += 1
-    if i % 3 == 0:
-        continue
-    print(i)
+n = int(input("n:"))
+i = 2
+if n > 1:
+    while i < (n // 2) + 1:
+        if n % i == 0:
+            print("is not")
+            break
+        i += 1
+    else:
+        print(n, "is a prime")
 else:
-    print("OK")
+    print(n, "is not a prime number")
