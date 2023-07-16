a57a08cc0def4f8a42bdd79ed31162113bd654fd
saeidyazdi7 <76227269+saeidyazdi7@users.noreply.github.com>
2023-07-16 15:36:06 +0330

    1

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-marks = []
-name = input("Enter name:")
-marks.append(name)
-scoreMath = float(input("mark:"))
-marks.append(scoreMath)
-scoreHistory = float(input("mark:"))
-marks.append(scoreHistory)
-scoreIT = float(input("mark:"))
-marks.append(scoreIT)
-ave = (marks[1] + marks[2] + marks[3]) / 3
-print(ave)
+a = [1, 2, 3]
+b = ["a", "b", "c"]
+a[-1:] = b
+print(a)
