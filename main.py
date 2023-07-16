5ed3c82e0065e9266d8b02203ec2e7f29ed44774
saeidyazdi7 <76227269+saeidyazdi7@users.noreply.github.com>
2023-07-16 17:48:30 +0330

    1

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
-x = int(input("Enter:"))
-if x % 5 == 0 and x % 2 == 0:
-    print("Yes")
+x = int(input("Enter x:"))
+y = int(input("Enter y:"))
+z = int(input("Enter z:"))
+if x < y + z and y < x + z and z < x + y:
+    print("میتواند مثلث باشد")
+    if x == y and y == z:
+        print("متساوی الاضلاع")
+    if x == y or y == z or x == z:
+        print("متساوی الساقین")
+    if x != y and y != z and x != z:
+        print("مختلف الاضلاع")
+    if x ^ 2 == y ^ 2 + z ^ 2 or y ^ 2 == x ^ 2 + z ^ 2 or z ^ 2 == y ^ 2 + x ^ 2:
+        print("قائم الزاویه")
 else:
-    print("No")
+    print("نمیتواند مثلث باشد")
