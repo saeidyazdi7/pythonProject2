0d906a91ff0f50660a471250c6a45f7559d1f51f
saeidyazdi7 <76227269+saeidyazdi7@users.noreply.github.com>
2023-07-16 15:30:23 +0330

    1

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
-ch = input("Enter your char :")
-print(ord(ch))
+marks = []
+name = input("Enter name:")
+marks.append(name)
+scoreMath = float(input("mark:"))
+marks.append(scoreMath)
+scoreHistory = float(input("mark:"))
+marks.append(scoreHistory)
+scoreIT = float(input("mark:"))
+marks.append(scoreIT)
+ave = (marks[1] + marks[2] + marks[3]) / 3
+print(ave)
