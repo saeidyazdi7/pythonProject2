70251e439a7405b4db7007de34f5f5b5cfd4cf37
saeidyazdi7 <76227269+saeidyazdi7@users.noreply.github.com>
2023-07-16 17:30:05 +0330

    2

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-mark = int(input("Mark :"))
-if 15 <= mark <= 20:
-    print("A")
-elif 10 <= mark < 15:
-    print("B")
-elif 5 <= mark < 10:
-    print("C")
-elif 0 <= mark < 5:
-    print("D")
+x = int(input("Enter:"))
+if x % 5 == 0 and x % 2 == 0:
+    print("Yes")
 else:
-    print("Hosh!")
+    print("No")
