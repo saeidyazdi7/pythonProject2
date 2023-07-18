32d79c5d5afb5353cce48c3a463b9062e0f8de81
saeidyazdi7 <76227269+saeidyazdi7@users.noreply.github.com>
2023-07-18 15:47:41 +0330

    2

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 x = int(input("x:"))
 y = int(input("y:"))
 m = min(x, y)
-tmp = 1
-for i in range(1, m + 1):
+
+for i in range(m, 0, -1):
     if x % i == 0 and y % i == 0:
-        if i > tmp:
-            tmp = i
-print(tmp)
+        print(i)
+        break
