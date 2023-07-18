acf00d0838f0fb46e36214986a433989a94758cc
saeidyazdi7 <76227269+saeidyazdi7@users.noreply.github.com>
2023-07-18 17:37:11 +0330

    2

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
-from random import choice
-
-x = ["a", "b", "c"]
-y = x.copy()
 while True:
-    computer = choice(y)
-    answer = input(f"your guess  {computer} ? (y/n) :")
-    if "y" in answer.lower():
-        print("Computer Win!")
+    print("select you option:\n\tEncrypt\n\tDecrypt\n\tExit")
+    choice = input("Your choice:")
+    if choice == "Encrypt":
+        plain_text = input("Text:")
+        encrypted_text = ""
+        for c in plain_text:
+            x = ord(c) * 2 + 5
+            encrypted_text += chr(x)
+        print("encrypted_text :", encrypted_text)
+        print("*" * 40)
+
+    elif choice == "Decrypt":
+        encrypted_text = input("Text:")
+        plain_text = ""
+        for c in encrypted_text:
+            x = (ord(c) - 5) // 2
+            plain_text += chr(x)
+        print("plain_text :", plain_text)
+        print("*" * 40)
+    elif choice == "Exit":
+        print("Bye")
         break
-    y.remove(computer)
+    else:
+        print("wrong!")
