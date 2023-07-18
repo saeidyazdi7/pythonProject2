b264e26faf52f03b33aa8ae7eecb418617bd99e0
saeidyazdi7 <76227269+saeidyazdi7@users.noreply.github.com>
2023-07-18 18:43:36 +0330

    2

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
-import random, string
+import time
+from os import system
 
-lower = string.ascii_lowercase
-upper = string.ascii_uppercase
-letter = string.ascii_letters
-symbols = "!@#$%^&*()_+|{}:"
-numbers = "123456789"
-all = letter + lower + numbers + symbols
 while True:
-    print("Choose:\n\t1)create\n\t2)exit")
-    choice = input("Enter:")
-    if choice == "1":
-        length = int(input("Enter your length:"))
-        password = "".join((random.sample(all, length)))
-        print(password)
-    elif choice == "2":
+    choice = input("start?(y/n)")
+    if "y" in choice.lower():
+        hour = int(input("Enter:"))
+        minute = int(input("Enter:"))
+        second = int(input("Enter:"))
+        total = hour * 3600 + minute * 60 + second
+        print("timer strats now...")
+        time.sleep(1)
+        while total >= 1:
+            print(total)
+            total -= 1
+            time.sleep(1)
+            system("cls")
+        print("end")
+    elif 'n' in choice.lower():
+        print("Bye")
         break
     else:
         print("wrong!")
