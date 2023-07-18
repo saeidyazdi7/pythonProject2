40cea9220237ebe262fedd997e2af18d223898e8
saeidyazdi7 <76227269+saeidyazdi7@users.noreply.github.com>
2023-07-18 18:26:14 +0330

    2

--- main.py
+++ main.py
@@ -1,25 +1,19 @@
-while True:
-    print("select you option:\n\tEncrypt\n\tDecrypt\n\tExit")
-    choice = input("Your choice:")
-    if choice == "Encrypt":
-        plain_text = input("Text:")
-        encrypted_text = ""
-        for c in plain_text:
-            x = ord(c) * 2 + 5
-            encrypted_text += chr(x)
-        print("encrypted_text :", encrypted_text)
-        print("*" * 40)
+import random, string
 
-    elif choice == "Decrypt":
-        encrypted_text = input("Text:")
-        plain_text = ""
-        for c in encrypted_text:
-            x = (ord(c) - 5) // 2
-            plain_text += chr(x)
-        print("plain_text :", plain_text)
-        print("*" * 40)
-    elif choice == "Exit":
-        print("Bye")
+lower = string.ascii_lowercase
+upper = string.ascii_uppercase
+letter = string.ascii_letters
+symbols = "!@#$%^&*()_+|{}:"
+numbers = "123456789"
+all = letter + lower + numbers + symbols
+while True:
+    print("Choose:\n\t1)create\n\t2)exit")
+    choice = input("Enter:")
+    if choice == "1":
+        length = int(input("Enter your length:"))
+        password = "".join((random.sample(all, length)))
+        print(password)
+    elif choice == "2":
         break
     else:
         print("wrong!")
