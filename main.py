6cf057bdd8e4ff96357a51851bf446cb83611abf
saeidyazdi7 <76227269+saeidyazdi7@users.noreply.github.com>
2023-07-18 14:47:31 +0330

    2

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-n = int(input("enter number:"))
-for i in range(1, n + 1):
-    for j in range(1, i + 1):
-        print(j, end="")
-    print()
+from random import choice
+
+coin = ["shir", "khat"]
+shir = 0
+khat = 0
+for _ in range(1000):
+    r = choice(coin)
+    if r == "shir":
+        shir += 1
+    else:
+        khat += 1
+print(shir, "shir")
+print(khat, "khat")
