1bedac27f354e5c627db27ee15c849f9b06ea694
saeidyazdi7 <76227269+saeidyazdi7@users.noreply.github.com>
2023-07-22 19:16:10 +0330

    def

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ def max3():
     x = int(input("Enter:"))
     y = int(input("Enter:"))
     z = int(input("Enter:"))
-    return max(x, y, z)
+    print("Max:", max(x, y, z))
 
 
-print("Max:", max3())
+max3()
