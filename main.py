bc172dd94d4f81a0a573ebb8c3d173df940c0b21
saeidyazdi7 <76227269+saeidyazdi7@users.noreply.github.com>
2023-07-23 16:37:33 +0330

    def

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-def func(a, b, c):
+def func(a, b=0, *c, **d):
     print("a:", a)
     print("b:", b)
     print("c:", c)
+    print("d:", d)
 
 
-d = {'a': 1, 'b': 2, 'c': 3}
-func(**d)
+func(1, 2, 3, 4, 5, 6, 7, 8, c=50, y=100)
