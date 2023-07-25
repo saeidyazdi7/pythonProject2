7600ce09995fe32ad0afa3760bf5cc6cf063fdf3
saeidyazdi7 <76227269+saeidyazdi7@users.noreply.github.com>
2023-07-25 17:42:13 +0330

    def

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-def my_max(lst):
-    max_element = lst[0]
+def my_min(lst):
+    min_element = lst[0]
     for element in lst[1:]:
-        if element > max_element:
+        if element < min_element:
             max_element = element
-    return max_element
+    return min_element
+
 
 lst = [5, 3, 7, 9, 2, 4]
-print(my_max(lst))
+print(min(lst))
