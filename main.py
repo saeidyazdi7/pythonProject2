ba6a1f3582f2de49b12c3c345393498cdf33f592
saeidyazdi7 <76227269+saeidyazdi7@users.noreply.github.com>
2023-07-25 16:58:57 +0330

    def

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
-x = 0
+def my_len(obj):  # تعریف یک تابع با نام my_len و یک پارامتر obj
+    count = 0  # ایجاد یک متغیر با مقدار صفر به عنوان شمارنده
+    for item in obj:  # حلقه for برای پیمایش در هر عضو obj
+        count += 1  # افزودن یک واحد به شمارنده
+    print(count)
+    return count  # برگرداندن مقدار شمارنده به عنوان خروجی تابع
 
 
-def a():
-    x = 9
-
-    def b():
-        nonlocal x
-        x += 1
-        print(x)
-
-    b()
-
-
-a()
-print(x)
+x = input("Enter:")
+my_len(x)
