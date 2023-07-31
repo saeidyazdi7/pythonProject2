e88c239987267c28fe6c1e5142d63e3bcc2c8ba3
saeidyazdi7 <76227269+saeidyazdi7@users.noreply.github.com>
2023-07-31 18:58:28 +0330

    lambda

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
-# Define a string and a character
-string = "Hello world"
-character = "H"
-
-# Use lambda function to check if the string starts with the character
-result = lambda x, y: x.startswith(y)
-
-# Print the result
-print("The string", string, "starts with the character", character, ":", result(string, character))
-
-# Output:
-# The string Hello world starts with the character H : True
+# # Define a string and a character
+# string = "Hello world"
+# character = "H"
+#
+# # Use lambda function to check if the string starts with the character
+# result = lambda x, y: x.startswith(y)
+#
+# # Print the result
+# print("The string", string, "starts with the character", character, ":", result(string, character))
+#
+# # Output:
+# # The string Hello world starts with the character H : True
+s = "SaeidYazdi"
+c = 'a'
+new1 = lambda x: x.isdigit()
+print("the string", s, "Start with", c, ":", new1(s))
