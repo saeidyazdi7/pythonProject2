c33e8264172a979227a1bd667173a112c6e354fb
saeidyazdi7 <76227269+saeidyazdi7@users.noreply.github.com>
2023-07-31 18:40:59 +0330

    lambda

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-dictionaries = [{"weight": 50, "apple": "red"}, {"weight": 40, "banana": "yellow"}, {"weight": 60, "orange": "orange"},
-                {"weight": 30, "lime": "green"}]
-new = sorted(dictionaries, key=lambda x: list(x.values())[1])
-print(new)
+# Define a string and a character
+string = "Hello world"
+character = "H"
+
+# Use lambda function to check if the string starts with the character
+result = lambda x, y: x.startswith(y)
+
+# Print the result
+print("The string", string, "starts with the character", character, ":", result(string, character))
+
+# Output:
+# The string Hello world starts with the character H : True
