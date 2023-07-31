2ad659660b9dd3e21843278c7ece2fa314d46f9b
saeidyazdi7 <76227269+saeidyazdi7@users.noreply.github.com>
2023-07-31 17:43:12 +0330

    lambda

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 number = [2, 3, 4, 5, 6, 7, 8, 9]
 new = list(filter(lambda y: y % 2 == 0, number))
-new1 = list(filter(lambda y: y % 2 != 0, number))
+new2 = list(filter(lambda y: y % 2 != 0, number))
 print(new)
-print(new1)
+print(new2)
