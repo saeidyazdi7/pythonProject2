7688e69d48c4c9269c7b68550f472fcc6ca2ff75
saeidyazdi7 <76227269+saeidyazdi7@users.noreply.github.com>
2023-07-31 18:02:24 +0330

    lambda

--- main.py
+++ main.py
@@ -1,5 +1,2 @@
-number = [2, 3, 4, 5, 6, 7, 8, 9]
-new = list(filter(lambda y: y % 2 == 0, number))
-new2 = list(filter(lambda y: y % 2 != 0, number))
-print(new)
-print(new2)
+tuples = [('ali', 23), ('saeid', 24), ('zahra', 98)]
+print(sorted(tuples, key=lambda x: x[1]))
