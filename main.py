42b00583c9e1accdfac115d183070cd92b858dbc
saeidyazdi7 <76227269+saeidyazdi7@users.noreply.github.com>
2023-08-02 11:23:31 +0330

    decorator

--- main.py
+++ main.py
@@ -1,16 +1,33 @@
-# # Define a string and a character
-# string = "Hello world"
-# character = "H"
-#
-# # Use lambda function to check if the string starts with the character
-# result = lambda x, y: x.startswith(y)
-#
-# # Print the result
-# print("The string", string, "starts with the character", character, ":", result(string, character))
-#
-# # Output:
-# # The string Hello world starts with the character H : True
-s = "SaeidYazdi"
-c = 'a'
-new1 = lambda x: x.isdigit()
-print("the string", s, "Start with", c, ":", new1(s))
+from time import perf_counter
+from functools import wraps
+
+
+def time_calc(func):
+    @wraps(func)
+    def wrapper_decorator(*args, **kwargs):
+        start_time = perf_counter()
+        value = func(*args, **kwargs)
+        end_time = perf_counter()
+        run_time = end_time - start_time
+        print("the runtime of", func.__name__, "is :", run_time)
+        return value
+
+    return wrapper_decorator
+
+
+@time_calc
+def A(y):
+    s = 0
+    for i in range(y):
+        s += i ** 2
+
+
+@time_calc
+def B(x):
+    fact = 1
+    for i in range(1, x + 1):
+        fact *= i
+
+
+A(10000000)
+B(10000000)
