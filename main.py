e68aadaa0b362b198d4be980606c0a1b594f8bde
saeidyazdi7 <76227269+saeidyazdi7@users.noreply.github.com>
2023-08-13 19:20:20 +0330

    cube

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-def cube(x):
-    return x ** 3
+def repeat(num, dig):
+    count = 0
+    while num > 0:
+        if num % 10 == dig:
+            count += 1
+        num //= 10
+    return count
 
 
-y = int(input("X:"))
-n = cube(y)
-print(n)
+number = int(input("X:"))
+digit = int(input("Y:"))
+print(digit, "repeat", repeat(number, digit), "times")
