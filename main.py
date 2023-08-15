9aa9e8aa351002a4314e3828aaa2ab410a8c8412
saeidyazdi7 <76227269+saeidyazdi7@users.noreply.github.com>
2023-08-15 15:47:26 +0330

    firstClass

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-def func(a):
-    print(a ** 2)
+def dec(list):
+    print(sorted(list))
 
 
-d = func
-d(6)
+def ced(list):
+    print(sorted(list, reverse=True))
+
+
+def edc(f, list):
+    f(list)
+
+
+edc(dec, [1, 4, 5, 6, 78, 32, 234, ])
