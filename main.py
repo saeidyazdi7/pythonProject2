a5ae6871a7234c2ddd36d82e94bb724ceadafba5
saeidyazdi7 <76227269+saeidyazdi7@users.noreply.github.com>
2023-08-15 16:47:04 +0330

    firstClass

--- main.py
+++ main.py
@@ -1,20 +1,11 @@
-def mysort(s):
-    def A(x):
-        print(sorted(x, reverse=True))
-    def error(z):
-        print("Error", z)
-    def B(y):
-        print(sorted(y))
-    if s == "a":
-        return A
-    elif s == "d":
-        return B
-    else:
-        return error
-
-
-action = input(":")
-
-func = mysort(action)
-
-func([5, 7, 8, 9, 10, 2, 3, 1])
+def a():
+    x=10
+    y=4
+    print(locals())
+def b():
+    x=0
+    y=7
+    print(locals())
+b()
+a()
+print(locals())
