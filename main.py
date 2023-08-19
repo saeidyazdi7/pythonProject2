689a60f4491933285b91274180ee28c08a8030c7
saeidyazdi7 <76227269+saeidyazdi7@users.noreply.github.com>
2023-08-19 20:07:53 +0330

    <>

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-def lens(x):
-    count = 0
-    for _ in x:
-        count += 1
-    return count
+def lens(*args):
+    m = args[0]
+    for i in args:
+        if i < m:
+            m = i
+    return m
 
 
-print(lens("saeid yazdi"))
+print(lens(2, 4, 6))
