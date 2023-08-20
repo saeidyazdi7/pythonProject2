80c7da86b9124e0cd30fd0ae9731fcde5f1bcbca
saeidyazdi7 <76227269+saeidyazdi7@users.noreply.github.com>
2023-08-20 10:47:42 +0330

    lambda

--- main.py
+++ main.py
@@ -1,13 +1,2 @@
-def show_type(ch):
-    if "0" <= ch <= "9":
-        print("Number")
-    elif "a" <= ch <= "z":
-        print("Upper")
-    elif "A" <= ch <= "Z":
-        print("Lower")
-    else:
-        print("Other")
-
-
-c = input("Enter:")
-show_type(c)
+a = lambda x: x ** 3
+print(a(6))
