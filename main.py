c74425579a266deed0732fe99108204fc8b43592
saeidyazdi7 <76227269+saeidyazdi7@users.noreply.github.com>
2023-08-20 16:49:59 +0330

    wraps

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
+from functools import wraps
+
+
 def star(func):
+    @wraps(func)
     def inner(*x, **y):
         print("x" * 20)
         func(*x, **y)
+
     return inner
 
 
 @star
 def msg(name):
+    """Saeid jigili"""
     print("i am", name)
 
 
+print(msg.__doc__)
+print(msg.__name__)
 msg("Saeid")
