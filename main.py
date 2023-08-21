602a4aff32e1fece3ed4217072678a32acde9958
saeidyazdi7 <76227269+saeidyazdi7@users.noreply.github.com>
2023-08-21 10:26:41 +0330

    generator

--- main.py
+++ main.py
@@ -1,33 +1,13 @@
-from functools import wraps
-from time import perf_counter
+def func(x):
+    print("saeid")
+    yield x ** 2
+    print("hallo")
+    yield 5
+    print("wie geht's?")
+    yield x - 2
 
 
-def time_calculate(func):
-    @wraps(func)
-    def wrapper_decorator(*args, **kwargs):
-        start_time = perf_counter()
-        value = func(*args, **kwargs)
-        end_time = perf_counter()
-        run_time = end_time - start_time
-        print("runtime:", func.__name__, "is", run_time)
-        return value
-
-    return wrapper_decorator
-
-
-@time_calculate
-def A(y):
-    s = 0
-    for i in range(y):
-        s += i ** 2
-
-
-@time_calculate
-def B(x):
-    fact = 1
-    for i in range(1, x + 1):
-        fact *= 1
-
-
-A(1000)
-B(100)
+x = func(6)
+print(next(x))
+print(next(x))
+print(next(x))
