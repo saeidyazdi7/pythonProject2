82a5fe477aea5d44d2af4077fa4631b17095a244
saeidyazdi7 <76227269+saeidyazdi7@users.noreply.github.com>
2023-08-21 10:36:55 +0330

    generator

--- main.py
+++ main.py
@@ -1,13 +1,8 @@
-def func(x):
-    print("saeid")
-    yield x ** 2
-    print("hallo")
-    yield 5
-    print("wie geht's?")
-    yield x - 2
+def my_gen():
+    for i in range(1000):
+        yield i ** 2
 
 
-x = func(6)
-print(next(x))
-print(next(x))
-print(next(x))
+g = my_gen()
+for i in g:
+    print(i)
