4b4818022f8cd382a62d3582cb75f5ba689494c6
saeidyazdi7 <76227269+saeidyazdi7@users.noreply.github.com>
2023-08-21 08:27:02 +0330

    dec

--- main.py
+++ main.py
@@ -1,21 +1,31 @@
 from functools import wraps
 
+users = {"saeid": "13579", "reza": "24680"}
+blacklist = {"saeid"}
 
-def star(func):
+
+def ban(func):
     @wraps(func)
-    def inner(*x, **y):
-        print("x" * 20)
-        func(*x, **y)
+    def inner(*args, **kwargs):
+        if args[0] in blacklist:
+            print("You are black")
+        else:
+            func(*args, **kwargs)
 
     return inner
 
 
-@star
-def msg(name):
-    """Saeid jigili"""
-    print("i am", name)
+@ban
+def print_password(username):
+    print(username, ":", users[username])
 
 
-print(msg.__doc__)
-print(msg.__name__)
-msg("Saeid")
+@ban
+def change_password(username, new_password):
+    users[username] = new_password
+    print(username, ":", users[username])
+
+
+print_password("saeid")
+change_password("reza", "2244668800")
+print(users)
