44ce6f02d7091363644d83a78b0818f901b4791a
saeidyazdi7 <76227269+saeidyazdi7@users.noreply.github.com>
2023-08-21 09:59:26 +0330

    dec

--- main.py
+++ main.py
@@ -1,31 +1,33 @@
 from functools import wraps
-
-users = {"saeid": "13579", "reza": "24680"}
-blacklist = {"saeid"}
+from time import perf_counter
 
 
-def ban(func):
+def time_calculate(func):
     @wraps(func)
-    def inner(*args, **kwargs):
-        if args[0] in blacklist:
-            print("You are black")
-        else:
-            func(*args, **kwargs)
+    def wrapper_decorator(*args, **kwargs):
+        start_time = perf_counter()
+        value = func(*args, **kwargs)
+        end_time = perf_counter()
+        run_time = end_time - start_time
+        print("runtime:", func.__name__, "is", run_time)
+        return value
 
-    return inner
-
-
-@ban
-def print_password(username):
-    print(username, ":", users[username])
+    return wrapper_decorator
 
 
-@ban
-def change_password(username, new_password):
-    users[username] = new_password
-    print(username, ":", users[username])
+@time_calculate
+def A(y):
+    s = 0
+    for i in range(y):
+        s += i ** 2
 
 
-print_password("saeid")
-change_password("reza", "2244668800")
-print(users)
+@time_calculate
+def B(x):
+    fact = 1
+    for i in range(1, x + 1):
+        fact *= 1
+
+
+A(1000)
+B(100)
