167915d79b0194242aab8747e8c7c3aea03045d4
saeidyazdi7 <76227269+saeidyazdi7@users.noreply.github.com>
2023-08-22 16:21:26 +0330

    memory

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
-def f(n):
-    if n < 3:
-        return
-    elif n % 3 == 0:
-        print(n)
-    f(n - 1)
+from functools import wraps
 
 
-f(10)
+def memoize(func):
+    memory = {}
+
+    @wraps(func)
+    def wrapper_decorator(n):
+        if n not in memory:
+            memory[n] = func(n)
+        return memory[n]
+
+    return wrapper_decorator
+
+
+@memoize
+def fib(n):
+    if n == 0 or n == 1:
+        return n
+    return fib(n - 1) + fib(n - 2)
+
+
+print(fib(300))
