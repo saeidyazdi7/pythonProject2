99d484e967c3fbb98d933f14ee17785669d79f5a
saeidyazdi7 <76227269+saeidyazdi7@users.noreply.github.com>
2023-08-22 18:07:27 +0330

    sum

--- main.py
+++ main.py
@@ -1,16 +1,9 @@
 # تعریف یک تابع بازگشتی به نام is_palindrome
-def is_palindrome(s):
-    # اگر رشته خالی باشد یا فقط یک حرف داشته باشد، True را برگردان
-    if len(s) <= 1:
-        return True
-    # در غیر این صورت، بررسی کن که اولین و آخرین حرف رشته با هم برابر هستند یا خیر
+def version(s):
+    if s < 10:
+        return s
     else:
-        # اگر برابر نباشند، False را برگردان
-        if s[0] != s[-1]:
-            return False
-        # اگر برابر باشند، باقی رشته را بدون اولین و آخرین حرف برای بررسی بفرست
-        else:
-            return is_palindrome(s[1:-1])
+        return s % 10 + version(s // 10)
 
 
-print(is_palindrome("123321"))
+print(version(1234))
