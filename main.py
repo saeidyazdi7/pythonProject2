a9d6df4b04320aa0562f1c85848e648cd8208735
saeidyazdi7 <76227269+saeidyazdi7@users.noreply.github.com>
2023-08-22 11:13:24 +0330

    send

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
-def sending():
-    print("ready")
-    while True:
-        name = yield
-        print("my name:", name)
+from functools import wraps
 
 
-o = sending()
-next(o)
-o.send("saeid")
-o.send("reza")
+def coo(func):
+    @wraps(func)
+    def start(*args, **kwargs):
+        gn = func()
+        next(gn)
+        return gn
+
+    return start
+
+
+@coo
+def my_gen():
+    # print("start")
+    for i in range(2):
+        name = yield i
+        print("my name", name)
+
+
+g = my_gen()
+g.send("saeid")
