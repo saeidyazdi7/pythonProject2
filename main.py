1ebe826a9fceae779dc64966179ecedf7cf7c794
saeidyazdi7 <76227269+saeidyazdi7@users.noreply.github.com>
2023-08-30 08:40:38 +0330

    ASCII

--- main.py
+++ main.py
@@ -1,10 +1,2 @@
-def func(x):
-    if x % 2 != 0:
-        return x
-    else:
-        return 0
-
-
-x = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-z = [func(j) for j in x]
-print(z)
+s = bytearray("a", "utf-8")
+print(type(s))
